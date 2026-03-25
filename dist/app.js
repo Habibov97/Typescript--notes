b220bef2bcbbc34bfde0f470f050e3e1590ee0e3
Habibov97 <najaff.habibov@gmail.com>
2026-03-25 10:13:12 +0400

    fix

--- dist/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const data = 5;
-console.log('Hello World!');
-//# sourceMappingURL=app.js.map
---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+const envPath = path.join(__dirname, '../../.env');
